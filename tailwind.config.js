279d159408695dc30fd34097835a5a65696f26ea
Jamal <sayedjamal.ahmadi@dci-student>
2023-11-13 15:08:39 +0100

    Installing & Configuring TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bg-header-footer": "var(--bg-header-footer)",
+        "bg-page-color": "var(--bg-page-color)",
+        "white-color": "var(--white-color)",
+        "font-family-color": "var(--font-family-color)"
+      },
+      fontFamily: {
+        "main-font": ["Chicle", "serif"],
+        "general-font": ["Open Sans", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
