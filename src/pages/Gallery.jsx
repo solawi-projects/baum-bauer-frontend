551996edb93e558ddad5aecc9d70ea3e5cab03c6
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-11-14 15:07:10 +0100

    news page is done(bugs need to be fixed)

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,4 +1,8 @@
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div>
+      
+    </div>
+  );
 };
 export default Gallery;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,4 +1,107 @@
+import { IoIosArrowForward } from "react-icons/io";
 const News = () => {
-  return <div>News</div>;
+  return (
+    <>
+      {/* 2-coloums grid to divide the page in two columns  */}
+      <div className="grid grid-cols-2 bg-bg-page-color">
+        <h1 className="mt-10 ml-20 font-main-font text-8xl">
+          <p>BL</p>
+          <p className="ml-7">OG-</p>
+          <p className="leading-tight">NEW</p>{/* leading class is used to fix the line height */}
+          <p className="leading-10">
+            S
+            <span className="text-xl">
+              <span className="">Latest News</span>
+              <div className="ml-12 leading-3"> and Updates</div>
+            </span>
+          </p>
+        </h1>
+
+        {/* end of first column */}
+
+        {/* the container of the second column */}
+        <div>
+          {/* cards container */}
+          <div className="m-5">
+            {/* the first card */}
+            <div className="m-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="src/assets/images/news1.jpg"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  the first news heading
+                </h5>
+                <p className=" text-xs pt-3 pb-4">April 2023</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                Read more <span className="pt-1">< IoIosArrowForward /></span> 
+            {/*     <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg> */}
+            </a>
+              </div>
+        
+            </div>
+
+            {/* the second card */}
+            <div className="m-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="src/assets/images/news2.jpg"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  the second news heading
+                </h5>
+                <p className=" text-xs pt-3 pb-4">March 2023</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                Read more <span className="pt-1">< IoIosArrowForward /></span>
+            {/*     <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg> */}
+            </a>
+              </div>
+              
+            </div>
+            {/* the third card */}
+            <div className="m-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="src/assets/images/news3.jpg"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  the third news heading
+                </h5>
+                <p className=" text-xs pt-3 pb-4">March 2023</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                Read more  <span className="pt-1">< IoIosArrowForward /></span>
+            {/*     <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg> */}
+            </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default News;
