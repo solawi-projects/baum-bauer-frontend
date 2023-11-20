8265f6891ca4f8cd57957bbc4c80218ca8c72ee8
Jamal <sayedjamal.ahmadi@dci-student>
2023-11-20 14:00:24 +0100

    Installing Axios with doing pagination

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const axiosApiInstance = axios.create();
+axiosApiInstance.defaults.baseURL = 'http://localhost:4000';
+axiosApiInstance.defaults.withCredentials = false;
+
+export default axiosApiInstance;
