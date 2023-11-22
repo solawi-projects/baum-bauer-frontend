085e976a408f78fc8ea3020c306f0dd86d1e5a71
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-11-22 22:55:57 +0100

    tree

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,14 +2,7 @@ import { useState } from "react";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 // eslint-disable-next-line react/prop-types
-const Pagination = ({
-  postPerPage,
-  totalpost,
-  paginate,
-  firsttree,
-  lasttree,
-  currentPage,
-}) => {
+const Pagination = ({postPerPage,totalpost,paginate,firsttree,lasttree,currentPage,}) => {
   console.log(totalpost);
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalpost / postPerPage); i++) {
