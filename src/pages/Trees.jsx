8a396e98576b2096eb798978c18c4221f0476c28
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-11-22 03:50:35 +0100

    added

--- src/pages/Trees.jsx
+++ src/pages/Trees.jsx
@@ -9,18 +9,20 @@ import { HiHome } from "react-icons/hi";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosSearch } from "react-icons/io";
 import { useState,useEffect } from "react";
-import { IoIosArrowDropleftCircle } from "react-icons/io";
-import { IoIosArrowDroprightCircle } from "react-icons/io";
-import axios from "../utils/axiosInstance";
 
+import axios from "../utils/axiosInstance";
+import Pagination from "../components/Pagination";
 
 import "../components/Tress.css";
 const Sponsor = () => {
   const aLinkValues = [{ linkTo: "/", linkIcon: HiHome, linkText: "Home" }];
   const daLinkValues = { linkText: "Trees" };
   const [tree,setTree] = useState([]);
+ const [currentPage,setCurrentPage] = useState(1);
+ const [postPerPage]=useState(4)
   const [err, setErr] = useState("");
-
+const lasttree=currentPage*postPerPage;
+const firsttree=lasttree-postPerPage+1
  /*  const data = [
     {
       id: 1,
@@ -133,9 +135,9 @@ const Sponsor = () => {
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
-  const getTrees = () => {
+  const getTrees = async() => {
     try {
-      axios
+     await axios
         .get(`/api/Tree/get`)
         .then((response) => {
           console.log("Res:", response);
@@ -158,7 +160,10 @@ const Sponsor = () => {
   useEffect(() => {
     getTrees();
   }, []);
-
+const indexofLastPost =currentPage*postPerPage;
+const indexofFirstPost=indexofLastPost-postPerPage;
+const currentPosts=tree.slice(indexofFirstPost,indexofLastPost)
+const paginate=(pageNumber)=>setCurrentPage(pageNumber)
   return (
     <div>
       <PageBreadcrumb activeLinks={aLinkValues} deActiveLink={daLinkValues} />
@@ -214,7 +219,7 @@ const Sponsor = () => {
         </div>
       </div>
       <div className=" flex  justify-center flex-wrap gap-10 pt-40 pb-40 ml-20 mr-20">
-        {tree.map((item, index) => (
+        {currentPosts.map((item, index) => (
           <div key={index} className="flex  pr items-center ">
             <div className="w-60 p-10 h-65 bg-white rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500">
               <img
@@ -238,99 +243,7 @@ const Sponsor = () => {
           </div>
         ))}
       </div>
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Previous
-          </a>
-          <a
-            href="#"
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Next
-          </a>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Showing <span className="font-medium">1</span> to{" "}
-              <span className="font-medium">10</span> of{" "}
-              <span className="font-medium">97</span> results
-            </p>
-          </div>
-          <div>
-            <nav
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-              aria-label="Pagination"
-            >
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Previous</span>
-                <IoIosArrowDropleftCircle
-                  className="h-5 w-5"
-                  aria-hidden="true"
-                />
-              </a>
-              {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a
-                href="#"
-                aria-current="page"
-                className="relative z-10 inline-flex items-center bg-green-900 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                1
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                2
-              </a>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                3
-              </a>
-              <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                ...
-              </span>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                8
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                9
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                10
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Next</span>
-                <IoIosArrowDroprightCircle
-                  className="h-5 w-5"
-                  aria-hidden="true"
-                />
-              </a>
-            </nav>
-          </div>
-        </div>
-      </div>
+    <Pagination postPerPage={postPerPage} currentPage={currentPage} totalpost={tree.length} paginate={paginate} firsttree={firsttree} lasttree={lasttree}/>
     </div>
   );
 };
