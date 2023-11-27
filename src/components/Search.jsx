0c45fb8b106dbb918c91b558f97144f395880bd0
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-11-27 14:56:06 +0100

    search implemented

---
+++ src/components/Search.jsx
@@ -0,0 +1,52 @@
+import {  useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+import axios from "../utils/axiosInstance";
+
+// eslint-disable-next-line react/prop-types
+export default function Search({updateTree}) {
+  const [searchParam,setSearchParam] = useState("");
+ const searchHandler  = async () => {
+  try {
+    const response = await axios.get(`/api/tree/search/${searchParam}`);
+    console.log('Search Item Front:',response.data);
+    if (response.status === 200) {  
+        console.log(response.data); 
+       updateTree(response.data);
+    }
+  } catch (error) {
+    console.error("Error fetching Trees:", error.message);
+    throw error;
+  }
+};
+
+
+  return (
+    <>
+      <div className="backdrop-search"></div>
+      <div className="py-10 h-20 bg-gray-100 px-2">
+        <div className="max-w-auto mx-auto rounded-lg overflow-hidden md:max-w-xl md:max-h-m sm:max-w-xs">
+          <div className="md:flex">
+            <div className="w-full p-3">
+              <div className="relative md:max-h-m">
+                <i className="absolute fa fa-search text-gray-100 top-5 left-4"></i>
+                <input
+                  type="text"
+                  className="bg-white h-14 w-full px-12 rounded-lg focus:outline-none hover:cursor-pointer"
+                  name=""
+                  defaultValue={searchParam}
+                  onChange={(event) => setSearchParam(event.target.value)}
+                ></input>
+                <button
+                  className="absolute top-4 right-5 border-l pl-4"
+                  onClick={searchHandler}
+                  >
+                  <IoIosSearch className="iconsearch" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
